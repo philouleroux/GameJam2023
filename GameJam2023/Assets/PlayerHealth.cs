296b225166c4de21a64863499515ae66f130f99b
PEPE\etien <etienneperinet@hotmail.ca>
2023-03-11 10:57:19 -0500

    LightDone

--- GameJam2023/Assets/PlayerHealth.cs
+++ GameJam2023/Assets/PlayerHealth.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class PlayerHealth : MonoBehaviour
+public class PlayerHealth : LightSource
 {
     Slider slider;
     public static Slider instance;
+
+    [SerializeField] float lerpSpeed = 1f;
+
+    private float valueForSlider = 1f;
+
     private void Awake()
     {
         slider = GetComponent<Slider>();
+        //lightIntensity / maxIntensity
     }
 
     public void UpdateHP()
     {
-        //slider.value = 
+        valueForSlider = lightIntensity / maxIntensity;
+    }
+
+    private void Update()
+    {
+        slider.value = Mathf.Lerp(slider.value, valueForSlider, lerpSpeed);
+        UpdateHP();
     }
 }
