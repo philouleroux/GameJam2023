73787b967536c859e178447e9dd54510ecfe6d1b
qwertypeople <qwertypeople@yahoo.com>
2023-03-12 12:15:54 -0400

    no message

--- GameJam2023/Assets/Scripts/LightSources/LightSource.cs
+++ GameJam2023/Assets/Scripts/LightSources/LightSource.cs
@@ -62,7 +62,7 @@ public class LightSource : MonoBehaviour
     {
         if (enemyInTrigger > 0 && IsLit)
         {
-           LightIntensity -= (enemyInTrigger * speedDecreasingIntensity* Time.deltaTime);
+           LightIntensity -= (enemyInTrigdwsger * speedDecreasingIntensity* Time.deltaTime);
             if (LightIntensity <= 0.0f)
             {
                 IsLit = false;
